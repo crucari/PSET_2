d7984a493a3fdb6dc15575f7f6644a599f193d42
Carina Cruz <lecruz@bu.edu>
2018-04-16 02:22:01 +0000

    initials and caesar

---
+++ caesar.c
@@ -0,0 +1,17 @@
+// Unencrypted text is generally called plaintext. Encrypted text is generally called ciphertext. And the secret used is called a key.
+
+// More generally, Caesar’s algorithm (i.e., cipher) encrypts messages by "rotating" each letter by k positions.
+// More formally, if p is some plaintext (i.e., an unencrypted message), pi is the ith character in p, and k is a secret key (i.e., a non-negative integer),
+// then each letter, ci, in the ciphertext, c, is computed as
+
+// ci=(pi+k)mod26
+
+// Steps:
+// 1) Get the key
+        // -get it using the commandline arguement
+        // -atoi
+
+        //argc and argv
+// 2) Get the plain text
+// 3) Encipher
+// 4) Print cipher text
